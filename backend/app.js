05e1096f2f88bd0dbdde99179c9c37690d20436e
Pavel Fursov <fursov.pv@gmail.com>
2022-12-16 22:27:01 +0100

    Users and admins routes

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options('*', cors())
@@ -14,6 +15,7 @@ app.options('*', cors())
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -88,6 +88,7 @@ router.post('/login', async (req, res) => {
         const token = jwt.sign(
             {
                 userId: user.id,
+                isAdmin: user.isAdmin
             },
             secret,
             { expiresIn: '1d'}
@@ -97,6 +98,31 @@ router.post('/login', async (req, res) => {
     else {
         res.status(400).send('Password is incorrect');
     }
+})
+
+router.get(`/get/count`, async (req, res) => {
+
+    const userCount = await User.countDocuments()
+
+    if (!userCount) {
+        res.status(500).json({ success: false });
+    }
+    res.send({
+        count: userCount
+    });
+})
+
+router.delete(`/:id`, (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is deleted' });
+        }
+        else {
+            return res.status(404).json({ success: false, message: 'user not found' });
+        }
+    }).catch(err => {
+        return res.status(400).json({ success: false, error: err })
+    })
 })
 
 
